c020f296c49ca4732ed91589db546182afe11d0b
LimaNat <160279258+LimaNat@users.noreply.github.com>
2025-09-19 11:14:15 -0300

    Add files via upload

---
+++ qrcode/js/script.js
@@ -0,0 +1,31 @@
+//dom
+
+const url = document.querySelector('#url')
+const botao = document.querySelector('#botao')
+const qr = document.querySelector('#qrcode');
+
+//eventos
+botao.addEventListener('click', gerador)
+url.addEventListener('keydown', (event) => {
+    if(event.key === 'Enter'){
+        gerador()
+    }
+})
+
+//função
+function gerador(){
+    texto = url.value
+    if(texto){
+        qr.innerHTML = ''
+        code = new QRCode(qr,{
+            text: texto,
+            width: 400,
+            height: 400,
+            colorDark: 'rgba(0,0,0,0)',
+            colorLight: 'black'
+        }
+        )
+    }else{
+        qr.innerHTML =''
+    }
+}
